ab896b895b72df2dd587046895839ca6e6084efd
IMRANDIL <aliimranadil2@gmail.com>
2023-04-16 14:03:34 +0530

    some more changes

--- src/components/VideoPlayerWithReact.tsx
+++ src/components/VideoPlayerWithReact.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import { toast } from "react-toastify";
 
 interface VideoPlayerProps {
   videoSrc: string;
@@ -8,10 +7,6 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer2: React.FC<VideoPlayerProps> = ({ videoSrc, onError }) => {
-  // const handlePlayerError = () => {
-  //   toast.error("Error streaming video", { autoClose: 1000 });
-  // };
-
   return (
     <div className="player-wrapper">
       <ReactPlayer
